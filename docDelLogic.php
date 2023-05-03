ab4eb621ed018282e762250e5d6ab6670f2bfe1a
Muhammad Usman Bin Farid <99407553+UsmanUet276@users.noreply.github.com>
2023-05-03 17:37:09 +0500

    ok

--- docDelLogic.php
+++ docDelLogic.php
@@ -7,27 +7,6 @@ $doc = db::getRecord("SELECT UserId, DocTitle FROM Documents WHERE DocId = '$id'
 $userId = $doc['UserId'];
 $docTitle = $doc['DocTitle'];
 
-<<<<<<< Updated upstream
-$query = "DELETE FROM metadata WHERE DocId = $id";
-$result = db::deleteRecord($query);
-
-$query = "DELETE FROM ApprovalApplications WHERE DocId = $id";
-$result = db::deleteRecord($query);
-$query = "DELETE FROM DocOldPassword WHERE DocId = $id";
-$result = db::deleteRecord($query);
-$query = "DELETE FROM DocPassword WHERE DocId = $id";
-$result = db::deleteRecord($query);
-$query = "DELETE FROM Documents WHERE DocId = $id";
-$result = db::deleteRecord($query);
-
-if($result){
-    header("Location: viewAllDocs.php?status=1");
-}
-else{
-    header("Location: viewAllDocs.php?status=0");
-}
-=======
 db::deleteDoc($userId, $docTitle);
 header("Location: viewAllDocs.php?status=1");
 ?>
->>>>>>> Stashed changes
